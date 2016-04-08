d24d815d897bd5208a3a8ad3481d7568b1afc847
Dante Kelly <dante.kelly97@gmail.com>
2016-04-08 04:32:15 -0700

    Implemented hamburger and started interactivity on skills

--- scripts/main.js
+++ scripts/main.js
@@ -25,6 +25,41 @@ var element = [];
   });
 //});
 */
-$('.array div *'').hover(function() {
-  $(this).addClass('animated');
+
+var someonesAnimated = false;
+var animatedPerson = [];
+
+$('.array div *').hover(function() {
+  var whatAmI = $(this).closest('.div');
+  if($(this).closest('div').not(".animated") && someonesAnimated == false) {
+    someonesAnimated = true;
+    animatedPerson.push($(this).closest('div'));
+    $(this).closest('div').addClass('animated');
+    $(this).children('p').css('display','block');
+    alert('First come first server!');
+  } else if($(this).closest('div').not(".animated")){
+    someonesAnimated = true;
+    animatedPerson[0].removeClass('animated');
+    animatedPerson[0].children('p').css('display','none');
+    animatedPerson.pop();
+    animatedPerson.push($(this).closest('div'));
+    $(this).closest('div').addClass('animated');
+    $(this).children('p').css('display','block');
+    alert('Moving someone else out');
+  } else {
+    alert('You are animated! You stay there!');
+  }
+}, 2000);
+
+
+$('#hamburger').on('click', function() {
+  $('nav').slideToggle();
+});
+
+$('body').click(function(e) {
+    if ($(e.target).closest('#hamburger').length === 0) {
+      if($("nav").is(":visible")) {
+        $("nav").slideToggle();
+      }
+    }
 });
