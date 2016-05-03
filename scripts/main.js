c59aca0e2ff6d11879fc5b720a4efb9d5a0d921e
Dante Kelly <dante.kelly97@gmail.com>
2016-05-02 22:24:23 -0700

    Fixed skills animating on mobile platforms. Added responsive sizing to jobSearch picture.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,26 @@
 var Animated = false;
 
+var resize = function () {
+  if(window.innerWidth <= 630) {
+    $('.animated').find('p').removeClass('text-animated');
+    $('.animated').removeClass('animated');
+  }
+};
+$(window).resize(resize);
+resize();
+
 // Skill Section logic
 $('.array img').hover(function() {
   var target = $(this).closest('div');
   var current = $('.animated');
-  if(target.not(".animated") && Animated == false) {
+
+  if(target.not(".animated") && Animated == false && window.innerWidth > 630) {
+    console.log('Sent ' + window.innerWidth);
     Animated = true;
     $(this).next('p').addClass('text-animated');
     target.addClass('animated');
-  } else if(target.not(".animated")){
+  } else if(target.not(".animated") && window.innerWidth > 630){
+    console.log('Sent ' + window.innerWidth);
     current.find('p').removeClass('text-animated');
     current.removeClass('animated');
     target.addClass('animated');
